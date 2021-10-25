3a449624c3f80e3f1829df10d31c06a386ad8fb4
Archana Kamath <68199391+archana-kamath@users.noreply.github.com>
2021-10-25 11:59:27 +0530

    Update index.js

--- lambdaCode/index.js
+++ lambdaCode/index.js
@@ -1,10 +1,11 @@
 const { downloadImage, resize, saveToS3 } = require('./utils');
 
-const bucket = 'lambda-images-bucket-1';
+const bucket = '<insert destination bucket name>';
 
 exports.handler = async (event) => {
-  const buf = await downloadImage(event.url);
+  const final_url = "<insert destination bucket name URL>" + event.Records[0].s3.object.key
+  const buf = await downloadImage(final_url);
   const resized = await resize(buf, 100, 100);
-  const key = await saveToS3(bucket, event.name, resized);
+  const key = await saveToS3(bucket, event.Records[0].s3.object.key, resized);
   return { key };
 };
